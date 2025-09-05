43285389546118da81fa1135de0dd1e40ede541d
Vanshi10 <vanshikajuneja04vj@gmail.com>
2025-09-05 16:09:02 +0530

    side pannel setting changed to phoneix

--- src/vs/workbench/contrib/void/browser/voidSettingsPane.ts
+++ src/vs/workbench/contrib/void/browser/voidSettingsPane.ts
@@ -49,7 +49,7 @@ class VoidSettingsInput extends EditorInput {
 	}
 
 	override getName(): string {
-		return nls.localize('phoenixSettingsInputsName', 'Phoenix\'s Settings');
+		return nls.localize('voidSettingsInputsName', 'Phoenix Settings');
 	}
 
 	override getIcon() {
@@ -112,7 +112,7 @@ class VoidSettingsPane extends EditorPane {
 
 // register Settings pane
 Registry.as<IEditorPaneRegistry>(EditorExtensions.EditorPane).registerEditorPane(
-	EditorPaneDescriptor.create(VoidSettingsPane, VoidSettingsPane.ID, nls.localize('PhoenixSettingsPane', "Phoenix\'s Settings Pane")),
+	EditorPaneDescriptor.create(VoidSettingsPane, VoidSettingsPane.ID, nls.localize('VoidSettingsPane', "Phoenix Settings Pane")),
 	[new SyncDescriptor(VoidSettingsInput)]
 );
 
@@ -123,7 +123,7 @@ registerAction2(class extends Action2 {
 	constructor() {
 		super({
 			id: VOID_TOGGLE_SETTINGS_ACTION_ID,
-			title: nls.localize2('phoenixSettings', "Phoenix: Toggle Settings"),
+			title: nls.localize2('voidSettings', "Phoenix: Toggle Settings"),
 			icon: Codicon.settingsGear,
 			menu: [
 				{
@@ -172,7 +172,7 @@ registerAction2(class extends Action2 {
 	constructor() {
 		super({
 			id: VOID_OPEN_SETTINGS_ACTION_ID,
-			title: nls.localize2('phoenixSettingsAction2', "Phoenix: Open Settings"),
+			title: nls.localize2('voidSettingsAction2', "Phoenix: Open Settings"),
 			f1: true,
 			icon: Codicon.settingsGear,
 		});
@@ -202,7 +202,7 @@ MenuRegistry.appendMenuItem(MenuId.GlobalActivity, {
 	group: '0_command',
 	command: {
 		id: VOID_TOGGLE_SETTINGS_ACTION_ID,
-		title: nls.localize('phoenixSettingsActionGear', "Phoenix\'s Settings")
+		title: nls.localize('voidSettingsActionGear', "Phoenix Settings")
 	},
 	order: 1
 });
